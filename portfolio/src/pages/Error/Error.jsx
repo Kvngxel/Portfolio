5db0c6770e138ba57e9752cf07d3436b76ea4456
kvngxel <exceleze45@gmail.com>
2023-12-24 18:59:45 +0100

    Finished making nav, footer and error page mobile responsive - Portfolio is fully mobile responsive up to this point

--- portfolio/src/pages/Error/Error.jsx
+++ portfolio/src/pages/Error/Error.jsx
@@ -6,8 +6,8 @@ export const Error = () => {
     <div className="flex bg-[#ede7de] pt-10 pb-40 h-[100vh] justify-center items-center">
         <Nav/>
         <div className='mt-32'>
-            <p className='text-center text-[16vh] text-pry leading-none font-bold font-acron'>Page not found.</p>
-            <p className='text-center text-[16vh] text-pry leading-none font-bold font-acron mt-5'>Error :) 404.</p>
+            <p className='text-center text-[5vh] md:text-[16vh] text-pry leading-none font-bold font-acron'>Page not found.</p>
+            <p className='text-center text-[6vh] md:text-[16vh] text-pry leading-none font-bold font-acron mt-5'>Error :) 404.</p>
         </div>
     </div>
   )
