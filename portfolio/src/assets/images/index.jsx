553c4942bafc08d17dd0aa1d92776ebf60935d09
kvngxel <exceleze45@gmail.com>
2024-01-10 16:27:42 +0100

    Fixed portfolio Image and added spotify svg's

--- portfolio/src/assets/images/index.jsx
+++ portfolio/src/assets/images/index.jsx
@@ -2,10 +2,15 @@ import locale from './locale.png'
 import simon from './simon.png'
 import design from './design.png'
 import figma from './figma.png'
+import Excel from './Excel.jpg'
+import Excel2 from './Excel2.jpg'
+
 
 export {    
     locale,
     simon,
     design,
-    figma
+    figma,
+    Excel,
+    Excel2
 }
