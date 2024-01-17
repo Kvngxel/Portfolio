d61a71d35a19be0c895644b9f7dd8f64282b654b
kvngxel <exceleze45@gmail.com>
2024-01-17 11:52:10 +0100

    Finished About page

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -7,8 +7,9 @@ import { Excel, Excel2 } from '../../assets/images'
 export const About = () => {
   return (
     <div className="bg-[#ede7de] pt-10 pb-40">
-       <Nav/>
-       <div className='md:my-36'>
+      <Nav/>
+      {/* Main */}
+      <div className='md:my-36'>
         <div className='mt-[30%] mb-[7%] md:mt-32 md:mb-2'>
             <p className='pb-1 md:pb-0 text-center text-[5vh] md:text-[17vh] text-pry leading-none font-bold md:font-bold font-acron'>I'm Excel.</p>
         </div>
@@ -38,6 +39,7 @@ export const About = () => {
           </div>
           {/* Top Right */}
           <div className='md:w-[70%] md:mx-10 md:px-12 max-sm:mt-10'>
+          {/* Remember to make the temperature the exact temperature of where you are */}
             <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[7vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>I'm a Fullstack Developer working remotely from 26°C Abuja, Nigeria.</p>
             <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6'>
               In the last 5 years, I've excelled in digital design, focusing on front-end development,
@@ -47,16 +49,98 @@ export const About = () => {
               Currently, my emphasis is on spearheading design efforts for freelance projects. I specialize in enabling donors to directly support the freelance community</p>
           </div>
         </div>
-       </div>
-       <div className='mx-11 md:mx-48 md:mt-48 '>
-        <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[7vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>I'm open to collaboration if your focus aligns with growth, sustainability,
-          and promoting clean and efficient work practices.</p>
-        <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6 md:mr-10'>
+      </div>
+      {/* Collaboration */}
+      <div className='mx-11 md:mx-48 md:mt-48 '>
+      <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[7vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>I'm open to collaboration if your focus aligns with growth, sustainability,
+        and promoting clean and efficient work practices.</p>
+      <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6 md:mr-10'>
         Dedicated to leaving a positive impact on our Earth for future generations, my goal is to
         actively contribute to ideals of sustainability. If you share the same commitment, I would love
         to engage in a conversation with you.</p>
-       </div>
-       <Footer/>
+      </div>
+      {/* 1 - 4  */}
+      <div className='my-16 md:my-32 md:mx-20 py-16 md:py-32 px-8 md:pl-44 md:pr-32 bg-[#f1ece4] md:rounded-[70px] drop-shadow-sm'>
+        <div className='md:flex'>
+          <div className='mx-3'>
+            <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[5vh] text-pry leading-[120%] font-bold md:font-bold font-acron opacity-[.5]'>01</p>
+            <p className='mt-2 md:mt-6 md:pb-0 text-left text-[2.5vh] md:text-[4vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>Crafting Solutions</p>
+            <p className='mt-5 text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl md:mr-10'>I engage in the art of sketching wireframes and crafting prototypes.
+              The discourse around tangible designs, manifested in the browser, holds immeasurable value.
+              While design tools have their limits, the true realization comes with a link that unites
+              my team.
+            </p>
+          </div>
+          <div className='mx-3 max-sm:mt-10'>
+            <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[5vh] text-pry leading-[120%] font-bold md:font-bold font-acron opacity-[.5]'>02</p>
+            <p className='mt-2 md:mt-6 md:pb-0 text-left text-[2.5vh] md:text-[4vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>Fostering Team Harmony</p>
+            <p className='mt-5 text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl md:mr-10'>Great design doesn't emerge in isolation but flourishes in a collaborative
+              arena. It's a process of facilitation and iteration within a team. My design philosophy
+              revolves around involving stakeholders, cultivating a collaborative atmosphere that thrives
+              on constructive feedback.
+            </p>
+          </div>
+        </div>
+        <div className='md:flex mt-10 md:mt-20'>
+          <div className='mx-3'>
+            <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[5vh] text-pry leading-[120%] font-bold md:font-bold font-acron opacity-[.5]'>03</p>
+            <p className='mt-2 md:mt-6 md:pb-0 text-left text-[2.5vh] md:text-[4vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>Universal Accessibility Champion</p>
+            <p className='mt-5 text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl md:mr-1'>
+              Every design I embark upon aims for universal accessibility. It's not just a goal; it's the ethical
+              path to tread. Accessible designs are a boon for the masses, not the privileged few.
+            </p>
+          </div>
+          <div className='mx-3 md:ml-28 max-sm:mt-10'>
+            <p className='pb-1 md:pb-0 text-left text-[3vh] md:text-[5vh] text-pry leading-[120%] font-bold md:font-bold font-acron opacity-[.5]'>04</p>
+            <p className='mt-2 md:mt-6 md:pb-0 text-left text-[2.5vh] md:text-[4vh] text-pry leading-[120%] font-bold md:font-bold font-acron'>Eternal Innovation Journey</p>
+            <p className='mt-5 text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl md:mr-10'>
+              In my creative realm, everything is a canvas for change and experimentation. Not every stroke
+              will create a masterpiece, but the journey is worth the attempt. Each endeavor becomes a
+              lesson, a step towards continuous learning and improvement.
+            </p>
+          </div>
+        </div>      
+      </div>
+      {/* Final quote */}
+      <div className='mx-10 md:mx-[50vh]'>
+        <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6 md:mr-10'>Before I stepped into the world of product design, I was a web developer.
+          Specializing in full-stack development, I weave digital wonders on the World Wide Web, translating
+          ideas into interactive and visually appealing online experiences. With a keen eye for design and a
+          knack for problem-solving, I thrive on the ever-evolving challenges of the tech world.
+        </p>
+        <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6 md:mr-10'>Coding emails forced me to understand how HTML and CSS play together, a skill set
+          Beyond the lines of code, my heart beats for the thrill of sports, Whether it's the pulse-pounding
+          action on the field or the strategic maneuvers in a digital arena, I find joy in the diverse realms
+          of athletic pursuits. 
+        </p>
+        <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl my-6 md:mr-10'>
+          When the digital world and athletic delights take a backseat, you'll find me embracing the social
+          tapestry of life, relishing moments with friends as we explore the rich cultural tapestry of Abuja.
+          Whether it's a night out on the town or a cozy gathering at home, I believe in the power of
+          connection and shared experiences, weaving the threads of friendship into the fabric of my life's
+          narrative. So, join me on this exhilarating journey where technology meets passion, and every day
+          is an opportunity to create, savor, and celebrate!
+        </p>
+        <p className='text-pry font-semibold md:font-medium font-body text-[17px] md:text-xl mt-6 md:mr-10'>
+          See Abuja!!
+        </p>
+      </div>
+      {/* Bottom Images */}
+      <div className='flex mt-20 md:mt-32 md:mx-16'>
+        <div className='relative md:w-2/3 md:mx-2 md:rounded-3xl bg-white group overflow-hidden'>
+          <div className='bg-[#ede7de] text-pry rounded-lg ml-7 mb-5 px-5 py-2 h-10 absolute bottom-0 z-10 hidden group-hover:block'>
+            <p>Abuja, Nigeria</p>
+          </div>
+          <img src='https://i.pinimg.com/originals/4e/30/60/4e30608a2fcb1a6d5f6cfdb47b070cfd.jpg'/>
+        </div>
+        <div className='max-sm:hidden relative w-1/3 mx-2 rounded-3xl bg-white group overflow-hidden'>
+          <div className='bg-[#ede7de] text-pry rounded-lg ml-7 mb-5 px-5 py-2 h-10 absolute bottom-0 z-10 hidden group-hover:block'>
+            <p>Abuja, Nigeria</p>
+          </div>
+          <img className='w-full h-full object-cover' src='https://upload.wikimedia.org/wikipedia/commons/1/1b/Abuja_night_time.jpg'/>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
